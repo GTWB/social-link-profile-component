33a811ff5168241335a7eabf2790ff06de6cedf3
Giuseppe Tropea <giuseppetropea@Giuseppes-MacBook-Air.local>
2026-01-13 12:55:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import avatar from "./images/avatar-jessica.jpeg";
 
-function App() {
+/* const buttons = ["GitHub", "Frontend Mentor", "Linkedin", "X", "Instagram"]; */
+
+// Create Map Data
+
+const buttons = [
+  { key: "GitHub", value: "https://github.com/GTWB" },
+  { key: "Frontend Mentor", value: "https://www.frontendmentor.io/" },
+  { key: "Linkedin", value: "https://www.linkedin.com/" },
+  { key: "Instagram", value: "https://www.instagram.com/" },
+  { key: "X", value: "https://x.com/" },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile />
     </div>
   );
 }
 
-export default App;
+function Profile() {
+  return (
+    <div className="profile">
+      <img src={avatar} alt="avatar" />
+      <div>
+        <h3>Jessica Randall</h3>
+        <p style={{ color: "var( --text-secondary)", fontWeight: "600" }}>
+          London,United Kingdom
+        </p>
+      </div>
+
+      <p>"Front-end developer and avid reader"</p>
+
+      <div className="buttons">
+        {buttons.map(({ key, value }, i) => (
+          <Button key={i} value={value}>
+            {key}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Button({ children, value }) {
+  return <button onClick={() => window.open(value)}>{children}</button>;
+}
